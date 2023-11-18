f9c6141d34584a7ea4ca13cf9208742084104b4a
John Carlo <johncarlolabanero@gmail.com>
2023-11-19 02:39:30 +0800

    materializecss?

--- Learn_ReactJS/main/views.py
+++ Learn_ReactJS/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Learn1
 
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("Hello, World")
+    # return HttpResponse("Hello, World")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"Learn1":Learn1.objects.all})
