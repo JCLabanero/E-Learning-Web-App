4f975e3917de51549c03c557911cc16b0d899b59
John Carlo <johncarlolabanero@gmail.com>
2023-11-18 22:05:10 +0800

    update urls.py and preparing for models

--- Learn_ReactJS/main/urls.py
+++ Learn_ReactJS/main/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = "main"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',views.homepage, name="homepage"),
 ]
