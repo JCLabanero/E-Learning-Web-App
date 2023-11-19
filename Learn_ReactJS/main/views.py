0feb7fb32d7417900f27a6695d8fe0a075e92fcb
John Carlo <johncarlolabanero@gmail.com>
2023-11-20 03:39:29 +0800

    registering

--- Learn_ReactJS/main/views.py
+++ Learn_ReactJS/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Learn1
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -16,6 +17,16 @@ def login(request):
                   template_name="main/login.html")
 
 def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            user = form.save()
+            login(request,user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
     form = UserCreationForm
     return render(request,
                   "main/register.html",
